4d4698476765303da13d29e4d43b48b5f6c4ddad
Usmanhanafi <tholush@gmail.com>
2024-05-25 02:32:01 +0700

    update button component

--- pages/components/ButtonPrimary.js
+++ pages/components/ButtonPrimary.js
@@ -1,6 +1,6 @@
 const ButtonPrimary = ({ children, ...props }) => {
     return (
-        <div className='cursor-pointer text-md lg:text-xl font-semibold bg-primary px-8 py-3 rounded-full hover:bg-primary-dark' {...props}>{children}</div>
+        <button className='cursor-pointer text-md lg:text-xl font-semibold bg-primary px-8 py-3 rounded-full hover:bg-primary-dark' {...props}>{children}</button>
     )
 }
 
--- pages/components/ButtonPrimaryOutline.js
+++ pages/components/ButtonPrimaryOutline.js
@@ -1,6 +1,6 @@
 const ButtonPrimaryOutline = ({ children, color, ...props }) => {
     return (
-        <div className={`${color ? 'border-white/60 text-white hover:bg-secondary hover:text-text-white bg-secondary/30' : 'border-primary text-primary hover:bg-primary hover:text-text-primary bg-primary/10'} cursor-pointer text-md lg:text-xl border  px-8 py-3 rounded-full`} {...props}>{children}</div>
+        <buttib className={`${color ? 'border-white/60 text-white hover:bg-secondary hover:text-text-white bg-secondary/30' : 'border-primary text-primary hover:bg-primary hover:text-text-primary bg-primary/10'} cursor-pointer text-md lg:text-xl border  px-8 py-3 rounded-full`} {...props}>{children}</buttib>
     )
 }
 
